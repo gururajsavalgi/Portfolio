0a266d3a5bb994afbc14230e7251064c7fa3d8e6
Gururaj S Savalgi <89399330+gururajsavalgi@users.noreply.github.com>
2024-05-07 20:15:06 +0530

    Initial commit

---
+++ portfolio/static/javascript/main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings : ["Pythonist",'Django Developer','Frontend Developer'],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop:true
+})
